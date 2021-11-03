50c0d12ebd15a59ec767905e7c30349f96ead03d
NataliaSprinceana <natalia_sprinceana@yahoo.com>
2021-11-03 17:07:21 -0500

    terraform files for wordpress

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidrs" {}
+
+variable "private_subnet_cidrs" {}
+
+variable "instance_type" {}
+
+variable "PATH_TO_PRIVATE_KEY" {}
+
+variable "PATH_TO_PUBLIC_KEY" {}
+
+variable db_username {}
+
+variable db_password {}
